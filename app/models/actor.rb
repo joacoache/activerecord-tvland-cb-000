85528063dcd6ea0e313b688730a9924491dbd39a
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-18 15:01:48 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     array = []
-    self.characters.each {|i| array << i}
+    self.characters.each {|i| array << "#{i.name} - #{i.show.name}"}
     array
   end
 end
